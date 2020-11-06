3725f0ba3bb48b1bdc59dd153745924c9db74669
sumukshashidhar <sumukuuu@gmail.com>
2020-11-06 23:21:58 +0530

    fixed another :bug:

--- src/servercode/routes/main_routes.js
+++ src/servercode/routes/main_routes.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-    app.get('/', function(res) {
+    app.get('/', function(req, res) {
         res.status(200).json({
             "message":"All OK"
         });
