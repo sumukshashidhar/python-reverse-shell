8f4326734247e232c8439b53831fb1ecd2e6ffed
sumukshashidhar <sumukuuu@gmail.com>
2020-11-06 23:15:27 +0530

    added the dependencies

--- src/servercode/index.js
+++ src/servercode/index.js
@@ -1,1 +1,25 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const logger = require('./config/logger')
+const morgan = require('morgan')
+const rfs = require('rotating-file-stream') // version 2.x
+const path = require('path')
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+var accessLogStream = rfs.createStream('access.log', {
+    interval: '1d', // rotate daily
+    path: path.join(__dirname, 'log')
+})
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+
+require("./routes")(app);
+
+app.listen(3000, "0.0.0.0", function (req,res) {
+    logger.info("Server Started");
+});
